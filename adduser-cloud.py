a1c9500a476f3af319658256ccf4604af4fafd8d
albertomolina <alb.molina@gmail.com>
2012-10-19 18:01:56 +0200

    Pequeñas correcciones

--- adduser-cloud.py
+++ adduser-cloud.py
@@ -35,10 +35,11 @@ def test_usuario(usuarios_cloud,usuario):
 
 print ""
 print "Programa para añadir usuarios del LDAP al cloud"
-print "  Para añadir o modificar un solo usuario, utiliza:"
-print "  adduser-cloud <nombre-usuario>"
 print ""
-if raw_input("¿Seguro que quieres seguir (s/n)?") != 's':
+print "Para añadir o modificar un solo usuario, utiliza:"
+print "adduser-cloud <nombre-usuario>"
+print ""
+if raw_input("¿Seguro que quieres seguir (s/n)? ") != 's':
     sys.exit()
 
 # Se pasa como parámetro el nombre del usuario:
@@ -54,18 +55,18 @@ config = ConfigParser.ConfigParser()
 config.read("adduser-cloud.conf")
     
 #Realizamos la conexión con el LDAP:
-path = config.get("LDAP", "path")
-l = ldap.open(config.get("LDAP", "ldap_server"))
+path = config.get("ldap", "path")
+l = ldap.open(config.get("ldap", "ldap_server"))
 ldap_user = raw_input("Nombre de usuario del administrador del LDAP: ")
-ldap_user_atrib = config.get("LDAP","user_rdn_attrib")
-bind_user = "%s=%s,%s" (ldap_user_atrib, ldap_user, path)
-config.get("LDAP", "bind_dn")
+ldap_user_atrib = config.get("ldap","user_rdn_attrib")
+bind_user = "%s=%s,%s" % (ldap_user_atrib, ldap_user, path)
+config.get("ldap", "bind_dn")
 bind_pass = getpass("Contraseña: ")
 l.simple_bind_s(bind_user, bind_pass)
 # Definimos una lista con 3 atributos (rdn del usuario, mail y atributo en el
 # que se encuentra el hash SHA512 con sal)
-lista_atrib = [config.get("LDAP","user_rdn_attrib"),
-               'mail',config.get("LDAP","user_pass_attrib")]
+lista_atrib = [config.get("ldap","user_rdn_attrib"),
+               'mail',config.get("ldap","user_pass_attrib")]
 # Filtramos los objetos que son inetOrgPerson y tienen 
 filtro = '(&(objectClass=inetOrgPerson)(%s=*)(%s=%s))' % (lista_atrib[2],
                                                           lista_atrib[0],
@@ -99,7 +100,6 @@ for usuario in usuarios_ldap:
             if usuario_cloud["name"] == username:
                 nuevo_pass = usuario[1]["%s" % lista_atrib[2]][0]
                 userid = usuario_cloud["id"]
-                print nuevo_pass, userid
                 subprocess.call("keystone --token %s --endpoint %s user-password-update --pass %s %s" % (admintoken, url, nuevo_pass, userid), shell = True)
                 print "Actualizado el usuario %s" % username
                 break
@@ -117,12 +117,14 @@ for usuario in usuarios_ldap:
         payload2["tenant"]["description"] = "Proyecto personal de %s" % username
         payload2["tenant"]["enabled"] = True
         # Añadimos cada usuario a la base de datos de keystone:
-        nuevo_usuario = requests.post(url+'users', headers = cabecera,data=json.dumps(payload))
+        nuevo_usuario = requests.post(url+'users', headers = cabecera,
+                                      data=json.dumps(payload))
         if nuevo_usuario.status_code == 200:
             id_usuario = json.loads(nuevo_usuario.text)["user"]["id"]
             print "Creado el usuario %s con id %s" % (username, id_usuario)
         # Añadimos un tenant para cada usuario:
-            nuevo_proy = requests.post(url+'tenants', headers = cabecera,data=json.dumps(payload2))
+            nuevo_proy = requests.post(url+'tenants', headers = cabecera,
+                                       data=json.dumps(payload2))
             if nuevo_proy.status_code == 200:
                 id_proy = json.loads(nuevo_proy.text)["tenant"]["id"]
                 print "Creado el tenant con id %s" % id_proy
@@ -130,10 +132,9 @@ for usuario in usuarios_ldap:
         # sobre roles o asignar un rol a un usuario en un tenant, tenemos que hacer
         # esto con el cliente keystone :-/
     
-        # Ponemos el id de Member a "mano": 414fd98137754204bc61fad1d40cbdbc
-                member_id = "414fd98137754204bc61fad1d40cbdbc"
+        # En lugar de consultar el id del rol member, se lee del fichero de conf:
+                member_id = config.get("keystone", "member_id")
                 subprocess.call("keystone --token %s --endpoint %s user-role-add --user %s --role %s --tenant_id %s" % (admintoken, url, id_usuario, member_id, id_proy), shell = True)
         else:
             print "No se ha creado el usuario %s" % username
             continue
-        
