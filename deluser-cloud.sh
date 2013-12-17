a346bb4faddedcd4cf44415937662aed9a69f062
Migue Martin <miguegt@hotmail.es>
2013-12-17 12:13:48 +0100

    Contraseña no visible

--- deluser-cloud.sh
+++ deluser-cloud.sh
@@ -2,4 +2,5 @@
 #Definir la variable usuario con el nombre de usuario en el argumetno 1 del programa
 usuario=$1
 # Pedir la contrasenna de usuario y guardar en la variable passwd
-read -r -p 'Please enter your OpenStack Password: ' passwd
+read -s -p "Please enter your OpenStack Password: " passwd
+echo -e "\nDeleting user "$usuario"...."
