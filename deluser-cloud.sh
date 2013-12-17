ddc2326f0400d29c7d698dcd038b40571cee5617
Adrián Jiménez Blanes <ajblanes@gmail.com>
2013-12-17 12:05:30 +0100

    Update deluser-cloud.sh

--- deluser-cloud.sh
+++ deluser-cloud.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Definir la variable usuario con el nombre de usuario en el argumetno 1 del programa
+#Definir la variable usuario con el nombre de usuario en el argumento 1 del programa
 usuario=$1
 # Pedir la contrasenna de usuario y guardar en la variable passwd
 read -r -p 'Please enter your OpenStack Password: ' passwd
