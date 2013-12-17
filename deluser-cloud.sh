65f27c8bb9124a588bef6dcbb62fcf7fe16fb6c1
Migue Martin <miguegt@hotmail.es>
2013-12-17 12:00:39 +0100

    Definir nombre de usuario apra el programa

--- deluser-cloud.sh
+++ deluser-cloud.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Definir la variable usuario con el nombre de usuario en el argumetno 1 del programa
+usuario=$1
 # Pedir la contrasenna de usuario y guardar en la variable passwd
 read -r -p 'Please enter your OpenStack Password: ' passwd
-
