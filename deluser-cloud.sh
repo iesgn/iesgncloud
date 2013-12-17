240fb26803c362fbb53146d69445417c5ba4d40b
javier1973 <javi@debian>
2013-12-18 00:49:00 +0100

    creo funcion para borrar proyecto aun no esta terminada

--- deluser-cloud.sh
+++ deluser-cloud.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #Definir la variable usuario con el nombre de usuario en el argumento 1 del programa
 usuario=$1
 #Guardo la ubicación del fichero en la variable archivo por si la necesito más adelante
@@ -8,13 +9,14 @@ if [ -e $archivo ]
 # Si existe lo cargo y procedo a eliminar el usuario	
 then
 	source /openrc.sh
-	#Pasos que necesitamos en este programa(Asignarselos)
-	#borrar_grupos(Adrian Jimenez Blanes) -- Da un error cuando intentamos borrar un grupo 
+	
+	#Pasos que necesitamos en este programa(Asignarselos) -- Da un error cuando intentamos borrar un grupo 
 	#que esta en uso en alguna instancia. Por lo que habra que borrar antes la instancia.
+	#borrar_grupos(Adrian Jimenez Blanes) 
 	for i in `nova secgroup-list |grep -v ^\+|grep -v Name| awk '{print $2}'`; 
 	do `nova secgroup-delete $i` ; 
 	done
-
+	
 	#borrar_pares_de_claves
 	#borrar_subredes
 	#borrar_redes
@@ -26,7 +28,9 @@ then
 	#borrar_instantaneasvolumen
 	#borrar_imagenes
 	#borrar_instancias
-	#borrar_proyecto
+	#borrar_proyecto(Fracnisco Javier Gimenez)
+	borra_tenant()
+
 	#borrar_usuario(Miguel Angel Martin)
 	echo -e "\nDeleting user "$usuario"...."
 	keystone user-delete $usuario
@@ -36,16 +40,19 @@ echo -e "No existe el archivo /openrc.sh es necesario para borrar el usuario"
 exit 0
 fi
 
-# obtener ID de un usuario
-id=`keystone user-list | grep $1 |awk '{print $2}'`
-#obtener tenant de un usuario vale si usuario solo tiene un tenant
-tenant=`keystone tenant-list | grep $1 | awk '{print $2}'`
-#borrar proyecto
-keystone tenant-delete $tenant
-#creo un vector con los distintos proyectos del usuario
-tenants=(`keystone tenant-list | grep $1 | awk '{print $2}'`)
-#borro todos los proyectos de un usuario
-for i in '${tenants[*]}';
-do
-	keystone tenant-delete $i;
-done
+function borra_tenant():
+	#Estas lienas de código estan pendientes de confirmar la estrucatura del
+	#programa
+	# obtener ID de un usuario 
+	id=`keystone user-list | grep $1 |awk '{print $2}'`
+	#obtener tenant de un usuario vale si usuario solo tiene un tenant
+	tenant=`keystone tenant-list | grep $1 | awk '{print $2}'`
+	#borrar proyecto
+	keystone tenant-delete $tenant
+	#creo un vector con los proyectos del usuario
+	tenants=(`keystone tenant-list | grep $1 | awk '{print $2}'`)
+	#borro todos los proyectos de un usuario
+	for i in '${tenants[*]}';
+	do
+		keystone tenant-delete $i;
+	done
