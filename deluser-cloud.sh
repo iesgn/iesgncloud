3bb32a0ba37266e93b0a5f79155b99913b1de3dd
miguelmartin <miguegt@hotmail.es>
2013-12-18 10:04:08 +0100

    Update deluser-cloud.sh

--- deluser-cloud.sh
+++ deluser-cloud.sh
@@ -15,19 +15,19 @@ then
 	
 	#borrar_grupos(Adrian Jimenez Blanes)
 	for i in `nova secgroup-list |grep -v ^\+|grep -v Name| awk '{print $2}'`;
-	do `nova secgroup-delete $i` ;
+		do `nova secgroup-delete $i` ;
 	done
 	
 	#borrar_pares_de_claves(Carlos Miguel Hernandez Romero)(Funciona)
 	for i in `nova keypair-list |grep -v ^\+|grep -v Name| awk '{print $2}'`;
-	do `nova keypair-delete $i` ;
-	echo "Eliminada el par de claves" $i
+		do `nova keypair-delete $i` ;
+		echo "Eliminada el par de claves" $i
 	done
 	
 	#borrar_IPs_flotantes(Carlos Miguel Hernandez Romero)(Funciona)
 	for i in `nova floating-ip-list |grep -v ^\+|grep -v Ip| awk '{print $2}'`;
-	do `nova floating-ip-delete $i` ;
-	echo "Eliminada la IP flotante" $i
+		do `nova floating-ip-delete $i` ;
+		echo "Eliminada la IP flotante" $i
 	done
 	
         #borrar_subredes
@@ -46,8 +46,8 @@ then
 	keystone user-delete $usuario
 # Si no existe le indico a el usuario el problema
 else
-echo -e "No existe el archivo /openrc.sh es necesario para borrar el usuario"
-exit 0
+	echo -e "No existe el archivo /openrc.sh es necesario para borrar el usuario"
+	exit 0
 fi
 
 function borra_tenant():
