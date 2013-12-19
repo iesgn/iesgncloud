a8867528ca1598c9605cd1abfd1563fec7531e87
albertomolina <alb.molina@gmail.com>
2013-12-19 20:39:07 +0100

    Adding delrouter-cloud.py

---
+++ delrouter-cloud.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+from quantumclient.v2_0 import client as quantumc
+from keystoneclient.v2_0 import client as keystonec
+
+if len(sys.argv) == 2:
+    tenant_name = sys.argv[1]
+    print "Deleting routers from tenant %s " % tenant_name
+else:
+    print "You must provide tenant_name"
+    sys.exit()
+
+# Getting auth token from keystone
+try:
+    creds = get_keystone_creds()
+    keystone = ksclient.Client(**creds)
+except keystonec.exceptions.Unauthorized:
+    print "Invalid keystone username or password"
+    sys.exit()
+
+quantum = quantumc.Client('2.0',
+                          endpoint_url=config.get("quantum","endpoint"),
+                          token = keystone.auth_token)
+   
+tenant = keystone.tenants.find(name=tenant_name)
+    
+for router in quantum.list_routers(tenant_id=tenant.id)["routers"]:
+    quantum.remove_gateway_router(router["id"])
+    for port in quantum.list_ports(tenant_id)tenant.id)["ports"]:
+        if port["device_id"] == router["id"]:
+            quantum.remove_interface_router(router["id"],{port_id=port["id"]})
+            quantum.delete_router(router["id"])
+            break
+
