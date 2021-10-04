56f0165e70cf8bd01c34406be12899d9bf2253f8
Alberto Molina Coballes <alb.molina@gmail.com>
2021-10-04 21:41:11 +0200

    Keystone v3 with session and CA cert

--- createproject.py
+++ createproject.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
 Python script used to create projects and a network for every OpenStack
@@ -6,122 +6,126 @@ User from an LDAP directory. It requires previous LDAP configuration
 in keystone
 """
 import sys
-import ConfigParser
+import configparser
 import requests
+from keystoneauth1.identity import v3
+from keystoneauth1 import session
 from keystoneclient.v3 import client as keystonec
-from neutronclient.neutron import client as neutronc
+
+# from neutronclient.neutron import client as neutronc
 from credentials import get_keystone_v3_creds
 
 if len(sys.argv) == 2:
     user = sys.argv[1]
-    print "Adding user %s to OpenStack" % user
+    print ("Adding user %s to OpenStack" % user)
 else:
-    print """
+    print ("""
     If you want to create or modify ONLY ONE USER, please use:
-    $ adduser-cloud <username>
-    """
-    if raw_input("Are you sure you want to continue (y/n)? ") != 'y':
+    $ createproject <username>
+    """)
+    if input("Are you sure you want to continue (y/n)? ") != 'y':
         sys.exit()
     user = "*"
-    print "Adding or modifying ALL users"
-    
-config = ConfigParser.ConfigParser()
+    print ("Adding or modifying ALL users")
+
+config = configparser.ConfigParser()
 config.read("adduser-cloud.conf")
-    
-    
-# Getting auth token from keystone
+
+# Getting auth token from keystone with a session
 try:
     creds = get_keystone_v3_creds()
-    keystone = keystonec.Client(**creds)
+    auth = v3.Password(**creds)
+    sess = session.Session(auth=auth)
+    keystone = keystonec.Client(session=sess)
 except keystonec.exceptions.Unauthorized:
-    print "Invalid keystone username or password"
+    print ("Invalid keystone username or password")
     sys.exit()
 
 # Getting domain
-try:
-    domain = keystone.domains.find(
-        name=config.get("keystone","domain"))
-except keystonec.exceptions.NotFound:
-    print "Domain not found"
-    sys.exit()
-    
-user_list = keystone.users.list(domain = domain)
-    
-# For every user in list a new project is created
-for member in user_list:
-    # If user's project exists in keystone, user is ignored
-    try:
-        keystone.projects.find(name="Proyecto de %s" % member.name)
-        continue
-    except keystonec.exceptions.NotFound:
-        print "Creating new project for user %s" % member.name
+# try:
+#     domain = keystone.domains.find(
+#         name=config.get("keystone","domain"))
+# except keystonec.exceptions.NotFound:
+#     print "Domain not found"
+#     sys.exit()
 
-    # If user's project doesn't exist previously:
-    # - "Proyecto de <username>" project is created
-    # - user role is aggined to user in the project
-    # - a network with name "red de <username>" is created in the project
-    # - a subnet 10.0.0.0/24 is defined in the network
-    # - a router is created in the project
-    # - router gateway is defined in a external network previously created
-    # - router interface is created in subnet 10.0.0.0/24
+# user_list = keystone.users.list(domain = domain)
 
-    # Creating new project
-    project = keystone.projects.create(name = "Proyecto de %s" % member.name,
-                                       domain = domain,
-                                       description = "Proyecto de %s" % member.name)
-    print "Creating new project with id %s" % project.id
-    # Assigning selected role to member in the project with python requests
-    headers = {"X-Auth-Token":keystone.auth_token,
-               "Content-Type": "application/json"}
-    user_role = keystone.roles.find(name=config.get("keystone","role"))
-    url = "%s/projects/%s/users/%s/roles/%s" % (config.get("keystone","url"),
-                                                project.id,
-                                                member.id,
-                                                user_role.id)
-    try:
-        requests.put(url,headers=headers)
-        print "Assigning user role"
-    except requests.exceptions.RequestException as e:
-        print e
-        sys.exit(1)
-    # Assigning "heat_stack_owner" role to the user in the project
-    user_role = keystone.roles.find(name="heat_stack_owner")
-    url = "%s/projects/%s/users/%s/roles/%s" % (config.get("keystone","url"),
-                                                project.id,
-                                                member.id,
-                                                user_role.id)
-    try:
-        requests.put(url,headers=headers)
-        print "Assigning heat_stack_owner role"
-    except requests.exceptions.RequestException as e:
-        print e
-        sys.exit(1)
-    
-    neutron = neutronc.Client('2.0',
-                              endpoint_url=config.get("neutron","endpoint"),
-                              token = keystone.auth_token)
-    neutron.format = 'json'
-    network = {'name':'red de %s' % member.name,
-               'project_id': project.id,
-               'admin_state_up': True}
-    newnetwork = neutron.create_network({'network':network})
-    print "Creating new network with id %s" % newnetwork['network']['id']
-    subnet = {'network_id': newnetwork['network']['id'],
-              'ip_version':4,
-              'cidr':'10.0.0.0/24',
-              'enable_dhcp': True,
-              'project_id': project.id,
-              'dns_nameservers': ['%s' % config.get("neutron","dns_nameservers")]}
-    newsubnet = neutron.create_subnet({'subnet':subnet})
-    print "Creating new subnet with id %s" % newsubnet['subnet']['id']
-    router = {'name':'router de %s' % member.name,
-              'project_id': project.id,
-              'external_gateway_info':{'network_id':
-                                       config.get("neutron","external_net_id")},
-              'admin_state_up': True}
-    newrouter = neutron.create_router({'router':router})
-    print "Creating new router with id %s" % newrouter['router']['id']
-    neutron.add_interface_router(newrouter['router']['id'],
-                                 {'subnet_id': newsubnet['subnet']['id']})
-    print "Connecting router to subnet %s" % newsubnet['subnet']['id']
-    
+# # For every user in list a new project is created
+# for member in user_list:
+#     # If user's project exists in keystone, user is ignored
+#     try:
+#         keystone.projects.find(name="Proyecto de %s" % member.name)
+#         continue
+#     except keystonec.exceptions.NotFound:
+#         print "Creating new project for user %s" % member.name
+
+#     # If user's project doesn't exist previously:
+#     # - "Proyecto de <username>" project is created
+#     # - user role is aggined to user in the project
+#     # - a network with name "red de <username>" is created in the project
+#     # - a subnet 10.0.0.0/24 is defined in the network
+#     # - a router is created in the project
+#     # - router gateway is defined in a external network previously created
+#     # - router interface is created in subnet 10.0.0.0/24
+
+#     # Creating new project
+#     project = keystone.projects.create(name = "Proyecto de %s" % member.name,
+#                                        domain = domain,
+#                                        description = "Proyecto de %s" % member.name)
+#     print "Creating new project with id %s" % project.id
+#     # Assigning selected role to member in the project with python requests
+#     headers = {"X-Auth-Token":keystone.auth_token,
+#                "Content-Type": "application/json"}
+#     user_role = keystone.roles.find(name=config.get("keystone","role"))
+#     url = "%s/projects/%s/users/%s/roles/%s" % (config.get("keystone","url"),
+#                                                 project.id,
+#                                                 member.id,
+#                                                 user_role.id)
+#     try:
+#         requests.put(url,headers=headers)
+#         print "Assigning user role"
+#     except requests.exceptions.RequestException as e:
+#         print e
+#         sys.exit(1)
+#     # Assigning "heat_stack_owner" role to the user in the project
+#     user_role = keystone.roles.find(name="heat_stack_owner")
+#     url = "%s/projects/%s/users/%s/roles/%s" % (config.get("keystone","url"),
+#                                                 project.id,
+#                                                 member.id,
+#                                                 user_role.id)
+#     try:
+#         requests.put(url,headers=headers)
+#         print "Assigning heat_stack_owner role"
+#     except requests.exceptions.RequestException as e:
+#         print e
+#         sys.exit(1)
+
+#     neutron = neutronc.Client('2.0',
+#                               endpoint_url=config.get("neutron","endpoint"),
+#                               token = keystone.auth_token)
+#     neutron.format = 'json'
+#     network = {'name':'red de %s' % member.name,
+#                'project_id': project.id,
+#                'admin_state_up': True}
+#     newnetwork = neutron.create_network({'network':network})
+#     print "Creating new network with id %s" % newnetwork['network']['id']
+#     subnet = {'network_id': newnetwork['network']['id'],
+#               'ip_version':4,
+#               'cidr':'10.0.0.0/24',
+#               'enable_dhcp': True,
+#               'project_id': project.id,
+#               'dns_nameservers': ['%s' % config.get("neutron","dns_nameservers")]}
+#     newsubnet = neutron.create_subnet({'subnet':subnet})
+#     print "Creating new subnet with id %s" % newsubnet['subnet']['id']
+#     router = {'name':'router de %s' % member.name,
+#               'project_id': project.id,
+#               'external_gateway_info':{'network_id':
+#                                        config.get("neutron","external_net_id")},
+#               'admin_state_up': True}
+#     newrouter = neutron.create_router({'router':router})
+#     print "Creating new router with id %s" % newrouter['router']['id']
+#     neutron.add_interface_router(newrouter['router']['id'],
+#                                  {'subnet_id': newsubnet['subnet']['id']})
+#     print "Connecting router to subnet %s" % newsubnet['subnet']['id']
+
